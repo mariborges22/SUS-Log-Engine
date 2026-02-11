e7e57d10d600a4454a3a574ac37aa39e85a5b295
joel andrade <mariborgesdatascientist@gmail.com>
2026-02-11 14:48:35 -0300

    fix: skip existing bucket and target lock table

--- infra/bootstrap.tf
+++ infra/bootstrap.tf
@@ -3,27 +3,27 @@
 # Este arquivo cria os recursos necessários para o State Remoto e Lock.
 # ==============================================================================
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "nexus-sus-terraform-state"
-  
-  # Proteção contra deleção para evitar perda do histórico da infra
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name        = "Nexus-SUS-Terraform-State"
-    Environment = "Prod"
-    ManagedBy   = "Terraform-Bootstrap"
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "nexus-sus-terraform-state"
+#   
+#   # Proteção contra deleção para evitar perda do histórico da infra
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# 
+#   tags = {
+#     Name        = "Nexus-SUS-Terraform-State"
+#     Environment = "Prod"
+#     ManagedBy   = "Terraform-Bootstrap"
+#   }
+# }
+# 
+# resource "aws_s3_bucket_versioning" "terraform_state" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "nexus-sus-terraform-locks"
